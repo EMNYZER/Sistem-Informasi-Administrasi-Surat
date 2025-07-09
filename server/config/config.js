6cedc4591d0712b7e113c56ccdc4fde90831aa3c
Hilman Zulqarami <zulqaramih@gmail.com>
2025-07-09 19:50:45 +0800

    fix backend build

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   production: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASS || null,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT || "mysql",
